e5bef48102dce7bcf06004651adb11fe5274ea2f
Guido De Rosa <job@guidoderosa.net>
2011-01-14 00:14:05 +0100

    sqlauthinstance::connection_string

--- dansguardian/src/authplugins/sql.cpp
+++ dansguardian/src/authplugins/sql.cpp
@@ -3,22 +3,20 @@
 // Please refer to http://dansguardian.org/?page=copyright2
 // for the license for this code.
 
-//  This program is free software; you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation; either version 2 of the License, or
-//  (at your option) any later version.
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
 //
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the
-//  GNU General Public License for more details.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the
+// GNU General Public License for more details.
 //
-//  You should have received a copy of the GNU General Public License
-//  along with this program; if not, write to the Free Software
-//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA	02111-1307	USA
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
-
-// INCLUDES
 #ifdef HAVE_CONFIG_H
 	#include "dgconfig.h"
 #endif
@@ -37,7 +35,6 @@
 
 
 // GLOBALS
-
 extern bool is_daemonised;
 extern OptionContainer o;
 
@@ -45,8 +42,9 @@ extern OptionContainer o;
 class sqlauthinstance:public AuthPlugin
 {
 public:
-	// keep credentials for the whole of a connection - IP isn't going to change.
-	// not quite true - what about downstream proxy with x-forwarded-for?
+	// Keep credentials for the whole of a connection - IP isn't going to 
+	// change. Not quite true - what about downstream proxy with 
+	// x-forwarded-for?
 	sqlauthinstance(ConfigVar &definition):AuthPlugin(definition)
 	{
 		if (!o.use_xforwardedfor)
@@ -58,59 +56,28 @@ public:
 
 	int init(void* args);
 	int quit();
+private:
+	std::string connection_string;
 };
 
 // IMPLEMENTATION
 
-// class factory code *MUST* be included in every plugin
-
+// Class factory code *MUST* be included in every plugin
 AuthPlugin *sqlauthcreate(ConfigVar & definition)
 {
 	return new sqlauthinstance(definition);
 }
 
-// end of Class factory
-
-// 
-//
-// Standard plugin funcs
-//
-//
-
 int sqlauthinstance::quit() {
 	return 0;
 }
 
-// plugin init 
 int sqlauthinstance::init(void* args) {
-	/*
-	pool = new soci::connection_pool(poolSize);
-
-	char connection_string[1024];
-	sprintf(connection_string, "host='%s' db='%s' user='%s' password='%s'", 
-		cv["sqlauthdbhost"].c_str(), 
-		cv["sqlauthdbname"].c_str(), 
-		cv["sqlauthdbuser"].c_str(), 
-		cv["sqlauthdbpass"].c_str()
-	);
-#ifdef DGDEBUG
-	printf("sqlauth: %s connection string: %s\n", 
-			cv["sqlauthdb"].c_str(), connection_string);
-#endif
-	try {
-		for (size_t i = 0; i < poolSize; ++i) {
-			soci::session & sql = pool->at(i);
-			sql.open(cv["sqlauthdb"], connection_string);
-		}
-		return 0;
-	}
-	catch (std::exception const &e) {
-		if (!is_daemonised) 
-			std::cerr << "sqlauthinstance::init(): " << e.what() << '\n';
-		syslog(LOG_ERR, "sqlauthinstance::init(): %s", e.what());
-		return 1;
-	}
-	*/
+	connection_string = 
+		"host='"     + cv["sqlauthdbhost"] + "' " +
+		"db='"       + cv["sqlauthdbname"] + "' " + 
+		"user='"     + cv["sqlauthdbuser"] + "' " +
+		"password='" + cv["sqlauthdbpass"] + "'"  ;
 	return 0;
 }
 
@@ -128,20 +95,7 @@ int sqlauthinstance::identify(Socket& peercon, Socket& proxycon, HTTPHeader &h, 
 		ipstring = peercon.getPeerIP();
 	}
 
-	char connection_string[1024];
-	sprintf(connection_string, "host='%s' db='%s' user='%s' password='%s'", 
-		cv["sqlauthdbhost"].c_str(), 
-		cv["sqlauthdbname"].c_str(), 
-		cv["sqlauthdbuser"].c_str(), 
-		cv["sqlauthdbpass"].c_str()
-	);
 
-#ifdef DGDEBUG
-	printf("sqlauth: %s connection string: %s\n", 
-			cv["sqlauthdb"].c_str(), connection_string);
-#endif
-
-	
 	String sql_query( cv["sqlauthipuserquery"] );
 	sql_query.replaceall("-IPADDRESS-", ipstring.c_str());
 
